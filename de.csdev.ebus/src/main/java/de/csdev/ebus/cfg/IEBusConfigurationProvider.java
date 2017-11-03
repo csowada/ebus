2c8a3072fcf3892be52c15870602298bd94a964b
csowada <info@cs-dev.de>
2017-11-03 16:46:09 +0100

    Add new interface

---
+++ de.csdev.ebus/src/main/java/de/csdev/ebus/cfg/IEBusConfigurationProvider.java
@@ -0,0 +1,16 @@
+package de.csdev.ebus.cfg;
+
+import java.io.InputStream;
+import java.util.List;
+
+public interface IEBusConfigurationProvider {
+
+	public List<String> getConfigurationIds();
+
+	public String getConfigurationLabel(String configurationId);
+
+	public InputStream getConfigurationStream(String configurationId);
+
+//	public void loadConfiguration(IEBusConfigurationReader reader);
+	
+}
