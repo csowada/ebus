6cca9bf8c583a8f67cc9ec2815348134246efd21
Christian S. <git@cs-dev.de>
2018-11-03 15:40:06 +0100

    Change java version to 1.8

--- src/test/java/de/csdev/ebus/cfg/EBusNestedTemplatesTest.java
+++ src/test/java/de/csdev/ebus/cfg/EBusNestedTemplatesTest.java
@@ -43,84 +43,86 @@ public class EBusNestedTemplatesTest {
     @Before
     public void before() throws IOException, EBusConfigurationReaderException {
         commandRegistry = new EBusCommandRegistry(EBusConfigurationReader.class);
-        commandRegistry.loadCommandCollection(EBusNestedTemplatesTest.class.getResource("/nested-templates/FirstTemplate.json"));
-        commandRegistry.loadCommandCollection(EBusNestedTemplatesTest.class.getResource("/nested-templates/SecondTemplate.json"));
-        commandRegistry.loadCommandCollection(EBusNestedTemplatesTest.class.getResource("/nested-templates/ThirdCommand.json"));
+        commandRegistry.loadCommandCollection(
+                EBusNestedTemplatesTest.class.getResource("/nested-templates/FirstTemplate.json"));
+        commandRegistry.loadCommandCollection(
+                EBusNestedTemplatesTest.class.getResource("/nested-templates/SecondTemplate.json"));
+        commandRegistry.loadCommandCollection(
+                EBusNestedTemplatesTest.class.getResource("/nested-templates/ThirdCommand.json"));
     }
 
     @Test
     public void testIdentification() {
 
-        IEBusCommandMethod commandMethod = commandRegistry.getCommandMethodById("et",
-                "test.nextest-block", IEBusCommandMethod.Method.SET);
+        IEBusCommandMethod commandMethod = commandRegistry.getCommandMethodById("et", "test.nextest-block",
+                IEBusCommandMethod.Method.SET);
 
         assertNotNull("Command et.test.nextest-blockn not found!", commandMethod);
 
-        
         try {
             ByteBuffer buffer = EBusCommandUtils.buildMasterTelegram(commandMethod, (byte) 0x00, (byte) 0xFF, null);
             assertNotNull("Unable to compose byte buffer for command", buffer);
 
             logger.info(EBusUtils.toHexDumpString(buffer).toString());
-            
+
         } catch (EBusTypeException e) {
             logger.error("error!", e);
         }
 
     }
-    
+
     @Test
     public void testX() throws EBusTypeException {
 
-        IEBusCommandMethod commandMethod = commandRegistry.getCommandMethodById("et",
-                "test.tth", IEBusCommandMethod.Method.SET);
+        IEBusCommandMethod commandMethod = commandRegistry.getCommandMethodById("et", "test.tth",
+                IEBusCommandMethod.Method.SET);
 
         assertNotNull("Command et.test.tth not found!", commandMethod);
 
-        
         IEBusValue value = commandMethod.getMasterTypes().get(0);
         IEBusType<?> type = value.getType();
-        
-        Object decode = type.decode(new byte[] {(byte)0x90});
-        
+
+        @SuppressWarnings("unused")
+        Object decode = type.decode(new byte[] { (byte) 0x90 });
+
         Map<String, Object> values = new HashMap<String, Object>();
         values.put("tth", new GregorianCalendar());
-        
+
         try {
             ByteBuffer buffer = EBusCommandUtils.buildMasterTelegram(commandMethod, (byte) 0x00, (byte) 0xFF, values);
             assertNotNull("Unable to compose byte buffer for command", buffer);
 
             logger.info(EBusUtils.toHexDumpString(buffer).toString());
-            
+
         } catch (EBusTypeException e) {
             logger.error("error!", e);
         }
 
     }
-    
+
     @Test
     public void testY() throws EBusTypeException {
 
-        IEBusCommandMethod commandMethod = commandRegistry.getCommandMethodById("et",
-                "test.to", IEBusCommandMethod.Method.SET);
+        IEBusCommandMethod commandMethod = commandRegistry.getCommandMethodById("et", "test.to",
+                IEBusCommandMethod.Method.SET);
 
         assertNotNull("Command et.test.tth not found!", commandMethod);
 
-        
         IEBusValue value = commandMethod.getMasterTypes().get(0);
         IEBusType<?> type = value.getType();
-        
-        Object decode = type.decode(new byte[] {(byte)0x90});
-        
+
+        @SuppressWarnings("unused")
+        Object decode = type.decode(new byte[] { (byte) 0x90 });
+
         Map<String, Object> values = new HashMap<String, Object>();
         values.put("value", new GregorianCalendar());
-        
+
         try {
             ByteBuffer buffer = EBusCommandUtils.buildMasterTelegram(commandMethod, (byte) 0x00, (byte) 0xFF, values);
             assertNotNull("Unable to compose byte buffer for command", buffer);
 
             logger.info(EBusUtils.toHexDumpString(buffer).toString());
-            
+
         } catch (EBusTypeException e) {
             logger.error("error!", e);
         }
