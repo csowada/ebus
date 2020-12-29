a707b519b60336c43f83a91e5967c37da3fa7173
csowada <csowada@users.noreply.github.com>
2020-12-29 22:12:46 +0100

    remove unused imports

--- src/main/java/de/csdev/ebus/cfg/std/EBusConfigurationReader.java
+++ src/main/java/de/csdev/ebus/cfg/std/EBusConfigurationReader.java
@@ -22,9 +22,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
